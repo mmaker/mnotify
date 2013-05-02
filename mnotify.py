039cf172e10b4495ea1a88f6ae1742258def7c56
Michele Orrù <maker@python.it>
2013-05-02 09:29:44 +0200

    License bullshits (still warning on startup, wtf)

--- mnotify.py
+++ mnotify.py
@@ -9,24 +9,6 @@
 # based on:
 # growl.py
 # Copyright (c) 2011 Sorin Ionescu <sorin.ionescu@gmail.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
 
 # -----------------------------------------------------------------------------
@@ -40,13 +22,10 @@ from email.mime.text import MIMEText
 
 import weechat
 
-weechat.prnt('', 'mnotify: hello world!')
-
-
 SCRIPT_NAME = 'mnotify'
 SCRIPT_AUTHOR = 'maker'
-SCRIPT_VERSION = '0'
-SCRIPT_LICENSE = 'Beerware'
+SCRIPT_VERSION = '0.1'
+SCRIPT_LICENSE = 'Beerware License'
 SCRIPT_DESC = 'Sends mail notifications upon events.'
 
 # -----------------------------------------------------------------------------
